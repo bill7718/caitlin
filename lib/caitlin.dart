209c5f1c84cbf00f4c25915da238b8f45aa3a9ec
Bill <stokes7718@gmail.com>
2022-03-21 17:43:15 +0000

    base

--- lib/caitlin.dart
+++ lib/caitlin.dart
@@ -7,3 +7,4 @@ export 'src/single_child_scroll_scaffold.dart';
 export 'src/data_object_journey_form_state.dart';
 export 'src/my_app.dart';
 export 'src/simple_text_provider.dart';
+export 'src/mock_firebase_service.dart';
